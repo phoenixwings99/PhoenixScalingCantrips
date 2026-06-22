ce9c724db2c55a7b98fd2ddf08a4abacc9def98f
pheonixwings99 <infinitum99@gmail.com>
2026-06-22 14:22:12 -0400

    Fixed adding incorrect Ability for touch cantrips

--- PhoenixScalingCantrips/Spells/ProliferateCantrips.cs
+++ PhoenixScalingCantrips/Spells/ProliferateCantrips.cs
@@ -6,6 +6,7 @@ using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes.Spells;
 using Kingmaker.Enums.Damage;
 using Kingmaker.UnitLogic.FactLogic;
+using Kingmaker.UnitLogic.Parts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,72 +30,76 @@ namespace PhoenixScalingCantrips.Spells
 
             if (Settings.IsEnabled("addburningtouch"))
             {
-                AbilityConfigurator.For("BurningTouch").AddToSpellLists(0, SpellList.Magus, SpellList.Wizard).Configure();
+                AbilityConfigurator.For("BurningTouchCast").AddToSpellLists(0, SpellList.Magus, SpellList.Wizard).Configure();
+                SpellListConfigurator.For("4d72e1e7bd6bc4f4caaea7aa43a14639").AddToSpellsByLevel(new SpellLevelList(0) { m_Spells = new() { BlueprintTool.GetRef<BlueprintAbilityReference>("BurningTouchCast") } }).Configure();
                 if (Settings.IsEnabled("proliferatetoshaman"))
                 {
-                    FeatureConfigurator.For("faa12cd1a5ece2c408d3bd00803fe8a1").AddKnownSpell(characterClass: "145f1d3d360a7ad48bd95d392c81b38e", spell: "BurningTouch", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("faa12cd1a5ece2c408d3bd00803fe8a1").AddKnownSpell(characterClass: "145f1d3d360a7ad48bd95d392c81b38e", spell: "BurningTouchCast", spellLevel: 0).Configure();
                     
                 }
                 if (Settings.IsEnabled("proliferatetooracle"))
                 {
-                    FeatureConfigurator.For("3b68909df737cd4458509d7f3a9c3706").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "BurningTouch", spellLevel: 0).Configure();
-                    FeatureConfigurator.For("eac5d0fc2b143c247a42f790230eb778").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "BurningTouch", spellLevel: 0).Configure();
-                    FeatureConfigurator.For("804e1560f21c95b43a45a9825336bd19").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "BurningTouch", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("3b68909df737cd4458509d7f3a9c3706").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "BurningTouchCast", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("eac5d0fc2b143c247a42f790230eb778").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "BurningTouchCast", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("804e1560f21c95b43a45a9825336bd19").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "BurningTouchCast", spellLevel: 0).Configure();
                     
                 }
             }
             if (Settings.IsEnabled("addlessercorrosivetouch"))
             {
-                AbilityConfigurator.For("LesserCorrosiveTouch").AddToSpellLists(0, SpellList.Magus, SpellList.Wizard).Configure();
+                AbilityConfigurator.For("LesserCorrosiveTouchCast").AddToSpellLists(0, SpellList.Magus, SpellList.Wizard).Configure();
+                SpellListConfigurator.For("4d72e1e7bd6bc4f4caaea7aa43a14639").AddToSpellsByLevel(new SpellLevelList(0) { m_Spells = new() { BlueprintTool.GetRef<BlueprintAbilityReference>("LesserCorrosiveTouchCast") } }).Configure();
                 if (Settings.IsEnabled("proliferatetoshaman"))
                 {
                    
-                    FeatureConfigurator.For("5c3ccab7cb27f4a408531197eb2abd3f").AddKnownSpell(characterClass: "145f1d3d360a7ad48bd95d392c81b38e", spell: "LesserCorrosiveTouch", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("5c3ccab7cb27f4a408531197eb2abd3f").AddKnownSpell(characterClass: "145f1d3d360a7ad48bd95d392c81b38e", spell: "LesserCorrosiveTouchCast", spellLevel: 0).Configure();
                 }
                 if (Settings.IsEnabled("proliferatetooracle"))
                 {
-                    FeatureConfigurator.For("7bb4bb3e7fd26f34e8ca035a27e03e85").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "LesserCorrosiveTouch", spellLevel: 0).Configure();
-                    FeatureConfigurator.For("09106dcc03321154f955e2d0296a082e").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "LesserCorrosiveTouch", spellLevel: 0).Configure();
-                    FeatureConfigurator.For("2127c37ccc7986e4083665552d6ecd5c").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "LesserCorrosiveTouch", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("7bb4bb3e7fd26f34e8ca035a27e03e85").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "LesserCorrosiveTouchCast", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("09106dcc03321154f955e2d0296a082e").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "LesserCorrosiveTouchCast", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("2127c37ccc7986e4083665552d6ecd5c").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "LesserCorrosiveTouchCast", spellLevel: 0).Configure();
                 }
                
             }
             if (Settings.IsEnabled("addfrostytouch"))
             {
-                AbilityConfigurator.For("FrostyTouch").AddToSpellLists(0, SpellList.Magus, SpellList.Wizard).Configure();
+                AbilityConfigurator.For("FrostyTouchCast").AddToSpellLists(0, SpellList.Magus, SpellList.Wizard).Configure();
+                SpellListConfigurator.For("4d72e1e7bd6bc4f4caaea7aa43a14639").AddToSpellsByLevel(new SpellLevelList(0) { m_Spells = new() { BlueprintTool.GetRef<BlueprintAbilityReference>("FrostyTouchCast") } }).Configure();
                 if (Settings.IsEnabled("proliferatetoshaman"))
                 {
                     
-                    FeatureConfigurator.For("62f408b7561adb34b899363c905fa13a").AddKnownSpell(characterClass: "145f1d3d360a7ad48bd95d392c81b38e", spell: "FrostyTouch", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("62f408b7561adb34b899363c905fa13a").AddKnownSpell(characterClass: "145f1d3d360a7ad48bd95d392c81b38e", spell: "FrostyTouchCast", spellLevel: 0).Configure();
                 }
                 if (Settings.IsEnabled("proliferatetooracle"))
                 {
-                    FeatureConfigurator.For("e2f8bd7c45dfb954c8c42b168505c783").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "FrostyTouch", spellLevel: 0).Configure();
-                    FeatureConfigurator.For("d91dcec40f768ea47b45bb4c4b2af40d").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "FrostyTouch", spellLevel: 0).Configure();
-                    FeatureConfigurator.For("1267082c536549c4c86f0725b6eea627").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "FrostyTouch", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("e2f8bd7c45dfb954c8c42b168505c783").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "FrostyTouchCast", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("d91dcec40f768ea47b45bb4c4b2af40d").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "FrostyTouchCast", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("1267082c536549c4c86f0725b6eea627").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "FrostyTouchCast", spellLevel: 0).Configure();
                 }
                 //Frosty Touch to Winter (modded)
             }
             if (Settings.IsEnabled("addlessershockinggrasp"))
             {
-                AbilityConfigurator.For("LesserShockingGrasp").AddToSpellLists(0, SpellList.Magus, SpellList.Wizard).Configure();
+                AbilityConfigurator.For("FrostyTouchCast").AddToSpellLists(0, SpellList.Magus, SpellList.Wizard).Configure();
+                SpellListConfigurator.For("4d72e1e7bd6bc4f4caaea7aa43a14639").AddToSpellsByLevel(new SpellLevelList(0) { m_Spells = new() { BlueprintTool.GetRef<BlueprintAbilityReference>("FrostyTouchCast") } }).Configure();
                 if (Settings.IsEnabled("proliferatetoshaman"))
                 {
-                    FeatureConfigurator.For("5d810adea03fb644582eb74de32c75ec").AddKnownSpell(characterClass: "145f1d3d360a7ad48bd95d392c81b38e", spell: "LesserShockingGrasp", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("5d810adea03fb644582eb74de32c75ec").AddKnownSpell(characterClass: "145f1d3d360a7ad48bd95d392c81b38e", spell: "FrostyTouchCast", spellLevel: 0).Configure();
                     
                 }
                 if (Settings.IsEnabled("proliferatetooracle"))
                 {
-                    FeatureConfigurator.For("7c1fdd831af747b47bb2cce0051f309b").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "LesserShockingGrasp", spellLevel: 0).Configure();
-                    FeatureConfigurator.For("4fb980ffe85793b4194b11d3adb70d9e").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "LesserShockingGrasp", spellLevel: 0).Configure();
-                    FeatureConfigurator.For("3812dcb2fe59ecd449285391db677edc").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "LesserShockingGrasp", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("7c1fdd831af747b47bb2cce0051f309b").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "FrostyTouchCast", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("4fb980ffe85793b4194b11d3adb70d9e").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "FrostyTouchCast", spellLevel: 0).Configure();
+                    FeatureConfigurator.For("3812dcb2fe59ecd449285391db677edc").AddKnownSpell(characterClass: "20ce9bf8af32bee4c8557a045ab499b1", spell: "FrostyTouchCast", spellLevel: 0).Configure();
                 }
             }
             if (Settings.IsEnabled("joltformagus"))
             {
                 AbilityConfigurator.For("Jolt").AddToSpellLists(0, SpellList.Magus).Configure();
                 AbilityConfigurator.For("Ignition").AddToSpellLists(0, SpellList.Magus).Configure();
-            }
+                            }
             if (Settings.IsEnabled("adddissonanttouch"))
             {
                 if (Settings.IsCharOpPlusEnabled() && Settings.IsEnabled("sonictoarrowsong"))
@@ -107,7 +112,7 @@ namespace PhoenixScalingCantrips.Spells
             {
                 if (Settings.IsCharOpPlusEnabled() && Settings.IsEnabled("sonictoarrowsong"))
                 {
-                    FeatureConfigurator.For("23d3d21793cc4ae6a034860c89561253").AddKnownSpell(archetype: "b704d577abe54873b9228f56c2319b54", characterClass: "772c83a25e2268e448e841dcd548235f", spell: "DissonantTouch", 0).Configure(delayed: true);
+                    FeatureConfigurator.For("23d3d21793cc4ae6a034860c89561253").AddKnownSpell(archetype: "b704d577abe54873b9228f56c2319b54", characterClass: "772c83a25e2268e448e841dcd548235f", spell: "DissonantTouchCast", 0).Configure(delayed: true);
 
                 }
             }
